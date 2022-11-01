2fdba157244ba97edc18e774f1aef3010b22d9a1
Joshua Geisler <joshuageisler@Joshuas-MBP.home>
2022-11-01 12:06:28 -0400

    updated email and bio

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -20,16 +20,22 @@ const About = () => {
           </h2>
           <p>
             Blake Brunson joined Miles Redd’s acclaimed design firm in 2010
-            after graduating from the University of Alabama with a degree in Art
-            History. While influenced by Redd's use of color and sense of
-            traditional modernism, Brunson's unique voice showcases detailed
-            interiors with an edited, modern flair, leaving room for dramatic
-            features and showstopper pieces. Her work references her Southern
-            roots in Alabama, balancing a respect for history with irreverence
-            cultivated by her time in New York. In 2017, Brunson was named a
-            Next Wave Designer by House Beautiful. Brunson has written and
-            produced for House Beautiful and Architectural Digest. She lives in
-            New York.
+            after graduating from the University of Alabama, also her home
+            state, with a degree in Art History. During the 10 years she spent
+            under the influence of Redd’s use of color and sense of traditional
+            modernism, Brunson developed her own unique voice that showcases
+            detailed interiors with an edited, modern flair, leaving room for
+            dramatic features and showstopper pieces. In 2017, Brunson was named
+            a Next Wave Designer by House Beautiful.
+          </p>
+          <p>
+            In January 2021, Brunson opened her own residential interior design
+            firm, Blake Brunson Inc., which currently operates out of the
+            National Arts Club. Her work references her Southern roots in
+            Alabama, balancing a respect for history with irreverence cultivated
+            by her time in New York. Brunson has written and produced for House
+            Beautiful and Architectural Digest and had her work featured in the
+            Big Book of Chic. She lives in New York.
           </p>
           <p style={{ fontSize: "small", marginTop: "1em" }}>
             Photo by Jessica Antola
--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -1,42 +1,41 @@
-import React from 'react';
+import React from "react";
 import { AiOutlineInstagram, AiOutlineMail } from "react-icons/ai";
 import { ImBrightnessContrast } from "react-icons/im";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
-const notify = () => toast('Email copied to clipboard.');
+const notify = () => toast("Email copied to clipboard.");
 
 /*
-* THIS IS NECESSARY TO MAKE THE TOOLTIP WORK WITH CUSTOM CHILD ELEMENTS
-* https://material-ui.com/components/tooltips/
-* ignore the react strict mode warning in the console - it doesn't apply here
-*/
+ * THIS IS NECESSARY TO MAKE THE TOOLTIP WORK WITH CUSTOM CHILD ELEMENTS
+ * https://material-ui.com/components/tooltips/
+ * ignore the react strict mode warning in the console - it doesn't apply here
+ */
 
 export const ThemeToggle = React.forwardRef(function ThemeToggle(props, ref) {
   return (
     <span {...props} ref={ref}>
       <ImBrightnessContrast />
     </span>
-  )
+  );
 });
 export const Mail = React.forwardRef(function Mail(props, ref) {
   return (
-    <span 
-      {...props} 
-      ref={ref} 
+    <span
+      {...props}
+      ref={ref}
       onClick={() => {
-        navigator.clipboard.writeText("bb@blakebrunson.com ")
-          .then(notify)
+        navigator.clipboard.writeText("hello@blakebrunson.com").then(notify);
       }}
     >
       <AiOutlineMail />
       <Toaster />
     </span>
-  )
+  );
 });
 export const Insta = React.forwardRef(function Insta(props, ref) {
   return (
     <span {...props} ref={ref}>
       <AiOutlineInstagram />
     </span>
-  )
+  );
 });
